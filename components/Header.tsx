44c4b51fb1762e5559944937e4cd0dbaa338c334
Sunhee Kyung <48008003+shkyung@users.noreply.github.com>
2022-01-19 18:39:49 +0900

    create pages, apply titles, navigation and Link (#3)
    Co-authored-by: shkyung <shkyung@unopnd.com>

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+const Header = () => {
+    return (
+        <div>
+            <Link href="/">
+                <a>Home</a>
+            </Link>
+            <Link href="/features">
+                <a>Features</a>
+            </Link>
+            <Link href="/gallery">
+                <a>Gallery</a>
+            </Link>
+            <Link href="/news">
+                <a>News</a>
+            </Link>
+        </div>
+    )
+}
+
+export default Header
