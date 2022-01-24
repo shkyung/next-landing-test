32984e3e9dcf588f0767c497915d4e51288e9816
Sunhee Kyung <48008003+shkyung@users.noreply.github.com>
2022-01-24 16:23:38 +0900

    Fullpage test (#4)
    * set baseurl
    
    * add @fullpage/react-fullpage
    
    * add type.d.ts for @fullpage/react-fullpage
    
    * test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type {NextPage} from 'next'
 import styles from '../styles/Home.module.css'
 import Head from "next/head";
+import FullPage from "components/FullPage";
 
 const Home: NextPage = () => {
     return (
@@ -11,7 +12,7 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className={styles.main}>
-                <h1>Main Page</h1>
+                <FullPage/>
             </main>
         </div>
     )
